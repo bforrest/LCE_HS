189fcc8551391dae245b5b7b2259c74702a1b19e
Barry Forrest <barry.forrest@gmail.com>
2014-03-19 19:26:56 -0500

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // Program and Library for re-using.
+
+
+        // 3 fizz, 5 buzz, 8 bazz, 10 bar etc.
+        //Finish up test, modify program to support arbitrary tokens and divisors.
+        // cedric@headspring.com
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                var lib = new FizzBuzz.FizzBuzz(1, 1000000000);
+                foreach (var result in lib.Results())
+                {
+                    Console.WriteLine(result);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ FizzBuzz/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public FizzBuzz(int topEnd)
+            : this(0, topEnd)
+        {
+
+        }
+        public FizzBuzz(int start, int topEnd)
+        {
+            _topEnd = topEnd;
+            _start = start;
+
+            if( _topEnd < _start)
+            {
+                throw new ArgumentException("Start value is greater than the Top End value");
+            }
+            _results = new List<string>();
+        }
+
+        public IEnumerable<string> Results()
+        {
+            for (int i = _start; i <= _topEnd; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                   yield return "FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                   yield return "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    yield return "Buzz";
+                }
+                else
+                {
+                    yield return i.ToString();
+                }
+            }
+        }
+
+        private readonly int _topEnd;
+        private readonly int _start;
+        private readonly List<string> _results;
+    }
+}
